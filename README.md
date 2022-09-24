0dc288366fe9216d2f620f8816feae8823d46a60
Mayara <may.dnsantos@gmail.com>
2022-09-24 19:18:15 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Agenda com Django
+
+Primeiro projeto com Django feito para o curso de **[Python do Básico ao Avançado.](https://www.udemy.com/course/python-3-do-zero-ao-avancado/)** 
+
---
+++ contatos/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Contato, Categoria
+
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria', 'mostrar')
+    list_display_links = ('id', 'nome')
+    list_per_page = 10
+    search_fields = ('nome', 'telefone', 'email')
+    list_editable = ('telefone', 'mostrar')
+
+admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
+
