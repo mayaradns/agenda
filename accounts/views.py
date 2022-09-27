9ac837857b50fcacda19344559dd579218b4fcf7
Mayara <may.dnsantos@gmail.com>
2022-09-27 15:23:21 -0300

    criando sistema de login e cadastro

---
+++ accounts/views.py
@@ -0,0 +1,102 @@
+from email import message
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .models import FormContato
+
+
+def login(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+    
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+
+    user = auth.authenticate(request, username=usuario, password=senha)
+
+    if not user:
+        messages.error(request, 'Usuário ou senha inválidos.')
+        return render(request, 'accounts/login.html')
+    else:
+        auth.login(request, user)
+        messages.success(request, 'Você fez login com sucesso.')
+        return redirect('dashboard')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+def register(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/register.html')
+
+    nome = request.POST.get('nome')
+    sobrenome = request.POST.get('sobrenome')
+    email = request.POST.get('email')
+    usuario = request.POST.get('usuario')
+    senha = request.POST.get('senha')
+    senha2 = request.POST.get('senha2')
+
+    if not nome or not sobrenome or not email or not usuario or not senha or not senha2:
+        messages.error(request, 'Nenhum campo pode estar vazio.')
+        return render(request, 'accounts/register.html')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Email inválido.')
+        return render(request, 'accounts/register.html')
+
+    if len(senha) < 6:
+        messages.error(request, 'Senha precisa ter no mínimo 6 caracteres.')
+        return render(request, 'accounts/register.html')
+
+    if len(usuario) < 4:
+        messages.error(request, 'Usuário precisa ter no mínimo 4 caracteres.')
+        return render(request, 'accounts/register.html')
+
+    if senha != senha2:
+        messages.error(request, 'Senhas não conferem.')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(username=usuario).exists():
+        messages.error(request, 'Usuário já existe.')
+        return render(request, 'accounts/register.html')
+    
+    if User.objects.filter(email=email).exists():
+        messages.error(request, 'Email já existe.')
+        return render(request, 'accounts/register.html')
+
+    messages.success(request, 'Cadastrado com sucesso! Agora faça login.')
+
+    user = User.objects.create_user(username=usuario, email=email, password=senha,
+                                    first_name=nome, last_name=sobrenome)
+    user.save()
+    return redirect('login')
+
+
+@login_required(redirect_field_name='login')
+def dashboard(request):
+    if request.method != 'POST':
+        form = FormContato()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContato(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulário.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    descricao = request.POST.get('descricao')
+    if len(descricao) < 5:
+        messages.error(request, 'Descrição precisa ter mais que 5 caracteres.')
+        form = FormContato(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(request, f'Contato {request.POST.get("nome")} salvo com sucesso!')
+    return redirect('dashboard')
